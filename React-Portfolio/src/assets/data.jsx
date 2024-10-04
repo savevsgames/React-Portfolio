9e6c53b480d72d82fb24149a8039e24b016bb759
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-04 01:27:43 -0600

    Added Dummy data to data.jsx file

---
+++ React-Portfolio/src/assets/data.jsx
@@ -0,0 +1,92 @@
+import { FaHtml5, FaJs, FaTs, FaReact, FaPython } from "react-icons/fa";
+
+export const links = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#skills", text: "skills" },
+  { id: 4, href: "#projects", text: "projects" },
+];
+
+export const skills = [
+  {
+    id: 1,
+    title: "HTML&CSS",
+    icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
+    text: "Artistic and creative design skills, with a focus on responsive web design and a strong understanding of web standards and best practices",
+  },
+  {
+    id: 2,
+    title: "Javascript",
+    icon: <FaJs className="h-16 w-16 text-emerald-500" />,
+    text: "I love JavaScript, and I have a strong understanding of the language. I enjoy making the web interactive and dynamic in creative ways.",
+  },
+  {
+    id: 3,
+    title: "Typescript",
+    icon: <FaTs className="h-16 w-16 text-emerald-500" />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+  {
+    id: 4,
+    title: "React",
+    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+  {
+    id: 5,
+    title: "Python",
+    icon: <FaPython className="h-16 w-16 text-emerald-500" />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "https://react-projects.netlify.app/",
+    github: "https://github.com/savevsgames",
+    title: "first project",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "https://react-projects.netlify.app/",
+    github: "https://github.com/savevsgames",
+    title: "second project",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "https://react-projects.netlify.app/",
+    github: "https://github.com/savevsgames",
+    title: "third project",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "https://react-projects.netlify.app/",
+    github: "https://github.com/savevsgames",
+    title: "first project",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "https://react-projects.netlify.app/",
+    github: "https://github.com/savevsgames",
+    title: "second project",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "https://react-projects.netlify.app/",
+    github: "https://github.com/savevsgames",
+    title: "third project",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+];
---
+++ React-Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-sky-200">
+      <div className="mx-auto max-w-7xl px-8 py-2 flex flex-col sm:flex-row sm-gap-x-16 sm:items-center sm:py-8">
+        <h2 className="text-3xl font-bold">
+          <span className="text-sky-900">FULL-STACK </span> DEVELOPER
+        </h2>
+        <div className="flex gap-3">LINKS</div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
